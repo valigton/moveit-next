0f326a3d3a8b453eea5dc2b314a6d373625ea2ae
Valigton Verissimo <40119609+valigton@users.noreply.github.com>
2021-04-02 16:31:43 -0300

    criado contexto para manipular info

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,15 +1,40 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, ReactNode } from 'react';
+interface ChallengesContextData {
+  level: number; 
+  currentExperience: number; 
+  challengesCompleted: number; 
+  levelUp: () => void;
+  startNewChallenge: () =>void;
+}
 
-export const ChallengesContext = createContext({});
+export const ChallengesContext = createContext({} as ChallengesContextData);
+interface ChallengesProviderProps {
+  children: ReactNode;
+}
 
-export function ChallengesProvider({ children }) {
+export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [level, setLevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
 
   function levelUp() {
     setLevel( level + 1);
   }
+
+  function startNewChallenge() {
+    console.log("new challenge")
+  }
+
   return (
-    <ChallengesContext.Provider value={{ level, levelUp }}>
+    <ChallengesContext.Provider 
+      value={{ 
+        level, 
+        currentExperience, 
+        challengesCompleted, 
+        levelUp,
+        startNewChallenge 
+      }}
+    >
       { children }
     </ChallengesContext.Provider>
   )
