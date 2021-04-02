9786244455194a6561d3eb0763108f47fdcb5ed0
Valigton Verissimo <40119609+valigton@users.noreply.github.com>
2021-04-02 15:30:21 -0300

    criado context challenges

---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import Head from "next/head";
+
+import { CompletedChallenges } from "../components/CompletedChalenges";
+import { Countdown } from "../components/Countdown";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+
+import styles from "../styles/pages/Home.module.css";
+import { ChallengeBox } from "../components/ChallengeBox";
+
+export default function Home() {
+  return (
+    <div className={styles.conteiner}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
+      
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div>
+          <ChallengeBox />
+        </div>
+      </section>
+    </div>
+  );
+}
