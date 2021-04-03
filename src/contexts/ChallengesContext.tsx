674909109b63e152c0ce741e1722d0d369f0ec02
Valigton Verissimo <40119609+valigton@users.noreply.github.com>
2021-04-03 17:15:36 -0300

    Adicionado notificacao no navegador

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from 'react';
+import { createContext, useState, ReactNode, useEffect } from 'react';
 
 import challenges from '../../challenges.json';
 
@@ -16,6 +16,7 @@ interface ChallengesContextData {
   levelUp: () => void;
   startNewChallenge: () =>void;
   resetChallenge: () =>void;
+  completeChallenge: () =>void;
 }
 
 export const ChallengesContext = createContext({} as ChallengesContextData);
@@ -32,8 +33,12 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
   const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
+  useEffect(() => {
+    Notification.requestPermission();
+  }, []);
+
   function levelUp() {
-    setLevel( level + 1);
+    setLevel(level + 1);
   }
 
   function startNewChallenge() {
@@ -41,10 +46,36 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const challenge = challenges[randomChallengeIndex];
 
     setActiveChallenge(challenge);
+
+    new Audio('/notification.mp3').play();
+
+    if(Notification.permission === 'granted') {
+      new Notification('Novo Desafio', {
+        body: `Valendo ${challenge.amount} xp!`
+      })
+    }
   }
 
   function resetChallenge() {
     setActiveChallenge(null);
+  }
+
+  function completeChallenge() {
+    if(!activeChanllenge) {
+      return;
+    }
+    const { amount } = activeChanllenge;
+
+    let finalExperience = currentExperience + amount;
+
+    if(finalExperience >= experienceToNextLevel) {
+      finalExperience = finalExperience - experienceToNextLevel;
+      levelUp();
+    }
+
+    setCurrentExperience(finalExperience);
+    setActiveChallenge(null);
+    setChallengesCompleted(challengesCompleted + 1);
   }
 
   return (
@@ -57,7 +88,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         startNewChallenge,
         activeChanllenge,
         resetChallenge,
-        experienceToNextLevel 
+        experienceToNextLevel,
+        completeChallenge 
       }}
     >
       { children }
